c7aaa26a14829e6674f18b28c1b2c9e86e99bb8e
Christelle Bs <christelle.lbs34@gmail.com>
2024-05-07 10:34:31 +0200

    installation node.js - Jquery

--- themes/foce-child/js/script.js
+++ themes/foce-child/js/script.js
@@ -1,23 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() {
+//Effet fade in browserifysur les sections
+document.addEventListener("DOMContentLoaded", function () {
   // Sélectionne toutes les sections à observer
-  const sections = document.querySelectorAll('.banner, .story#story, #studio');
+  const sections = document.querySelectorAll(".banner, .story#story, #studio");
 
   // Créé un observer pour chaque section
-  const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-      // Si la section est visible, ajoute la classe 'show', sinon, retirez-la
-      if (entry.isIntersecting) {
-        entry.target.classList.add('show');
-        observer.unobserve(entry.target); // Arrête de surveiller la section une fois qu'elle est apparue
-      }
-    });
-  }, { threshold: 0.5 }); // Se déclenche lorsque 50% de la section est visible
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        // Si la section est visible, ajoute la classe 'show', sinon, retirez-la
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+          observer.unobserve(entry.target); // Arrête de surveiller la section une fois qu'elle est apparue
+        }
+      });
+    },
+    { threshold: 0.5 }
+  ); // Se déclenche lorsque 50% de la section est visible
 
   // Pour chaque section, commence à observer
-  sections.forEach(section => {
+  sections.forEach((section) => {
     observer.observe(section);
   });
 });
-
-
-
